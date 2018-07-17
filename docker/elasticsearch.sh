7ca4594923e8fe3d162250fd416c888194972a2c
chiragjn <jain.chirag925@gmail.com>
2018-07-17 21:45:39 +0530

    Match elasticsearch client and server versions for installation

--- docker/elasticsearch.sh
+++ docker/elasticsearch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ~/chatbot_ner_elasticsearch
 cd /tmp/
-curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.2.2.tar.gz
-tar -xzf elasticsearch-5.2.2.tar.gz -C ~/chatbot_ner_elasticsearch/
-~/chatbot_ner_elasticsearch/elasticsearch-5.2.2/bin/elasticsearch -d
+curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.3.0.tar.gz
+tar -xzf elasticsearch-5.3.0.tar.gz -C ~/chatbot_ner_elasticsearch/
+~/chatbot_ner_elasticsearch/elasticsearch-5.3.0/bin/elasticsearch -d
--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-~/chatbot_ner_elasticsearch/elasticsearch-5.2.2/bin/elasticsearch -Ees.insecure.allow.root=true -d
+~/chatbot_ner_elasticsearch/elasticsearch-5.3.0/bin/elasticsearch -Ees.insecure.allow.root=true -d
 /usr/sbin/nginx -g 'daemon off;'
