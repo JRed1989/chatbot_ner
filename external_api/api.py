3fbc456ca990c52bc50519b2d43b557501c3e05b
jayaraopratik <pratik.jayarao@haptik.co>
2018-08-02 18:28:51 +0530

    Add update_dictionary to handle the incoming update request

--- external_api/api.py
+++ external_api/api.py
@@ -1,5 +1,5 @@
 import json
-
+from chatbot_ner.config import ner_logger
 from django.http import HttpResponse
 from datastore.datastore import DataStore
 from external_api.external_api_utilities import structure_es_result
@@ -13,6 +13,7 @@ def get_entity_word_variants(request):
 
     """
     status = False
+    result = []
     try:
         dictionary_name = request.GET.get('dictionary_name')
         datastore_obj = DataStore()
@@ -21,5 +22,24 @@ def get_entity_word_variants(request):
         status = True
 
     except TypeError:
-        return HttpResponse(status=500)
+        ner_logger.debug('Error %s' % str(TypeError))
     return HttpResponse(json.dumps({'status': status, 'result': result}), content_type='application/json')
+
+
+def update_dictionary(request):
+    """This functionality initializes text detection functionality to detect textual entities.
+
+    Attributes:
+        request: url parameters
+
+    """
+    status = False
+    try:
+        dictionary_name = request.GET.get('dictionary_name')
+        dictionary_data = request.GET.get('dictionary_data')
+        datastore_obj = DataStore()
+        status = datastore_obj.external_api_update_entity(dictionary_name=dictionary_name,
+                                                          dictionary_data=dictionary_data)
+    except TypeError:
+        ner_logger.debug('Error %s' % str(TypeError))
+    return HttpResponse(json.dumps({'status': status}), content_type='application/json')
