a06d7bb1f2ccfd673ea20a842433abfa0b0894fa
jayaraopratik <pratik.jayarao@haptik.co>
2018-08-03 12:34:59 +0530

    Add json loads method

--- external_api/api.py
+++ external_api/api.py
@@ -2,7 +2,7 @@ import json
 from chatbot_ner.config import ner_logger
 from django.http import HttpResponse
 from datastore.datastore import DataStore
-from external_api.external_api_utilities import structure_es_result
+from external_api.external_api_utilities import structure_es_result, structure_external_api_json
 
 
 def get_entity_word_variants(request):
@@ -35,11 +35,13 @@ def update_dictionary(request):
     """
     status = False
     try:
-        dictionary_name = request.GET.get('dictionary_name')
-        dictionary_data = request.GET.get('dictionary_data')
+        word_entity_info = json.loads(request.body)
+        dictionary_name = word_entity_info['dictionary_name']
+        dictionary_data = word_entity_info['dictionary_data']
         datastore_obj = DataStore()
         status = datastore_obj.external_api_update_entity(dictionary_name=dictionary_name,
                                                           dictionary_data=dictionary_data)
+        structure_external_api_json(dictionary_data)
     except TypeError:
         ner_logger.debug('Error %s' % str(TypeError))
     return HttpResponse(json.dumps({'status': status}), content_type='application/json')
